b7b7cded12aedff6203ef4c45e1499a9d786c34c
Emerentius <emerentius@arcor.de>
2018-01-08 04:35:38 +0100

    add ability to generate sudokus, introduce subcommands

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
-CLI sudoku solver
+CLI sudoku solver and generator
+
+This exposes functionality of the rust sudoku library for testing purposes.
+The solver is reasonably fast, capable of solving sudokus in 5-200μs depending on difficulty on a 15W laptop cpu (i5 4210u).
+
+Generation happens in ~25μs (full grid) or ~400μs (uniquely solvable, minimal sudoku) on the same hardware.
+
+The best solvers known to me (ZSolve, fsss2) are ~4x as fast.
 
 ```bash
 $ echo '.816...9............4.376..6..4..5...3.....7...7..2..4..521.3............7...481.' \
 | sudoku
-
 281645793763928145594137628629473581438591276157862934845219367312786459976354812
+
+$ sudoku generate 2
+.....25..2...13....1.75....158...4....3.....8.....9.7...7.....152....7.....4.8.3.
+45.......7.94.......27.65...2...3.6.....6...5.....53.4876..2.....4...2.....9...1.
+
+$ sudoku generate 2 --solved
+371684592592713684846925137785461923423859761169372458658247319237196845914538276
+647395128952718643138426957379862514516934872824571396265187439791643285483259761
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,139 @@
 extern crate sudoku;
+#[macro_use]
 extern crate clap;
+extern crate isatty;
 
+use sudoku::Sudoku;
+use sudoku::parse_errors::LineFormatParseError;
+
+#[cfg(feature = "rayon")]
 extern crate rayon;
+#[cfg(feature = "rayon")]
 use rayon::prelude::*;
 
 use std::io::{self, Read, Write};
-use clap::{Arg, App};
+use clap::{Arg, App, SubCommand};
 
+#[cfg(feature = "rayon")]
 fn solve_and_print(input: &str) {
-    let stdout = std::io::stdout();
-    let mut lock = stdout.lock();
     let vec = input.par_lines()
         .map(sudoku::Sudoku::from_str_line)
         .map(|maybe_sudoku| {
-            maybe_sudoku.map(|sudoku| sudoku.solve_one())
+            maybe_sudoku.map(|sudoku| sudoku.solve_unique())
         })
         .collect::<Vec<_>>();
 
 
-    for sud in vec {
+    _print(vec.into_iter());
+}
+
+#[cfg(not(feature = "rayon"))]
+fn solve_and_print(input: &str) {
+    let sudokus = input.lines()
+        .map(sudoku::Sudoku::from_str_line)
+        .map(|maybe_sudoku| {
+            maybe_sudoku.map(|sudoku| sudoku.solve_unique())
+        });
+
+    _print(sudokus);
+}
+
+fn _print<I: Iterator<Item=Result<Option<Sudoku>, LineFormatParseError>>>(sudokus: I) {
+    let stdout = std::io::stdout();
+    let mut lock = stdout.lock();
+    for sud in sudokus {
         let _ = match sud {
-            Ok(Some(solution)) => write!(lock, "{}\n", solution.to_str_line()),
-            Ok(None) => write!(lock, "no solution\n"),
+            Ok(Some(solution)) => writeln!(lock, "{}", solution.to_str_line()),
+            Ok(None) => write!(lock, "no unique solution\n"),
             Err(_) => write!(lock, "invalid sudoku\n"),
         };
     }
 }
 
-fn main() {
-    let matches = App::new("sudoku")
-        .version("0.1")
-        .about("Solves sudokus")
-        .arg(Arg::with_name("sudoku_file")
-            .long("file")
-            .short("f")
-            .takes_value(true)
-            .value_name("FILE")
-        )
-        .get_matches();
+fn read_stdin(buffer: &mut String) {
+    let stdin = io::stdin();
+    let mut lock = stdin.lock();
+    let _ = lock.read_to_string(buffer);
+}
 
+fn main() {
+    let mut app = App::new("sudoku")
+        .version("0.2")
+        .about("Solves and generates sudokus")
+        .subcommand(
+            SubCommand::with_name("solve")
+                .about("Solve sudokus")
+                .arg(
+                    Arg::with_name("sudokus_file")
+                        .takes_value(true)
+                        .value_name("FILE")
+                )
+        )
+        .subcommand(
+            SubCommand::with_name("generate")
+                .about("Generate sudokus")
+                .arg(
+                    Arg::with_name("amount")
+                        .takes_value(true)
+                        .required(true)
+                )
+                .arg(
+                    Arg::with_name("solved")
+                        .help("generate solved sudokus")
+                        .long("solved")
+                )
+        );
+    let matches = app.clone().get_matches();
+
+    // FIXME: don't read all sudokus into buffer
+    //        read some in, process and output, read next in
     let mut sudoku_buffer = String::new();
 
-    if let Some(filename) = matches.value_of("sudoku_file") {
-        let mut file = std::fs::File::open(filename).unwrap_or_else(|e| panic!("Could not open file: {}", e));
-        let _ = file.read_to_string(&mut sudoku_buffer);
-    } else {
-        let stdin = io::stdin();
-        let mut lock = stdin.lock();
-        let _ = lock.read_to_string(&mut sudoku_buffer);
+    if let Some(matches) = matches.subcommand_matches("solve") {
+        if let Some(filename) = matches.value_of("sudokus_file") {
+            let mut file = match std::fs::File::open(filename) {
+                Ok(f) => f,
+                Err(e) => {
+                    println!("Could not open file: {}", e);
+                    return
+                }
+            };
+            let _ = file.read_to_string(&mut sudoku_buffer);
+        } else {
+            read_stdin(&mut sudoku_buffer);
+        }
+        solve_and_print(&sudoku_buffer);
+    } else if let Some(matches) = matches.subcommand_matches("generate") {
+        let amount = value_t_or_exit!(matches.value_of("amount"), usize);
+        let gen_sud = match matches.is_present("solved") {
+            true => Sudoku::generate_filled,
+            false => Sudoku::generate_unique,
+        };
+
+        #[cfg(feature = "rayon")]
+        (0..amount).into_par_iter()
+            .for_each(|_| {
+                println!("{}", gen_sud().to_str_line());
+            });
+
+        #[cfg(not(feature = "rayon"))]
+        {
+            let mut stdout = io::stdout();
+            let mut lock = stdout.lock();
+            for _ in 0..amount {
+                let _ = writeln!(lock, "{}", gen_sud().to_str_line());
+            }
+        }
+    } else if matches.subcommand.is_none() {
+        if !isatty::stdin_isatty() {
+            // if not operating interactively, read input from pipe and solve
+            // this isn't advertised in the help but it'll be a common "error"
+            // and be learned that way
+            read_stdin(&mut sudoku_buffer);
+            solve_and_print(&mut sudoku_buffer);
+        } else {
+            // print help
+            let _ = app.print_help();
+        }
     }
-    solve_and_print(&sudoku_buffer);
 }
